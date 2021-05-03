e2feef7b43dd0d71c4b72e483c97af6cad7dad92
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2021-05-03 15:34:12 -0700

    polymorph ip

--- src/_03_polymorphs/CircleMorph.java
+++ src/_03_polymorphs/CircleMorph.java
@@ -1,5 +1,25 @@
 package _03_polymorphs;
 
-public class CircleMorph {
+import java.awt.Color;
+import java.awt.Graphics;
 
+public class CircleMorph extends Polymorph{
+	private int circleIteration = 0;
+	
+	CircleMorph(int x, int y) {
+		super(x, y);
+	}
+	
+	public void update() {
+		circleIteration++;
+		
+		setX((int)(Math.cos(circleIteration % 360) * 100) + 100);
+		setY((int)(Math.sin(circleIteration % 360) * 100) + 100);
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(Color.green);
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
 }
